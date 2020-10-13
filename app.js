5ddc545b057f02d9066852e99304f9618c343821
ALU52 <69333284+ALU52@users.noreply.github.com>
2020-10-12 23:04:25 -0500

    Added timestamp command to make them readable *its really nice*

--- app.js
+++ app.js
@@ -300,6 +300,14 @@ client.on("message", (msg) => {
             msg.channel.send("```json\n" + s + "```")
             break;
 
+        case "timestamp":
+            if (!args[0]) { msg.reply("please supply a timestamp to translate"); return }
+            let num = args[0] * 1
+            if (num == NaN) { msg.reply("the timestamp should be in milliseconds since Jan 1, 1970"); return; } else {
+                msg.reply(timeDifference(args[0]))
+            }
+            break;
+
         default:
             //ignore unknown commands
             break;
@@ -623,6 +631,50 @@ function prompt(who, channel, text) {
         })
     })
 }
+
+/**
+ * Converts a timestamp to an approximate human-readable string
+ * Credit to fearofawhackplanet on stack overflow for this function
+ * found at https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time
+ * although, I removed the "current" argument because its pointless for the way I'll be using this
+ * @param {number} previous The previous timestamp
+ */
+function timeDifference(previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = Date.now() - previous;
+
+    if (elapsed < msPerMinute) {
+        return Math.round(elapsed / 1000) + ' seconds ago';
+    }
+
+    else if (elapsed < msPerHour) {
+        return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    }
+
+    else if (elapsed < msPerDay) {
+        return Math.round(elapsed / msPerHour) + ' hours ago';
+    }
+
+    else if (elapsed < msPerMonth) {
+        return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
+    }
+
+    else if (elapsed < msPerYear) {
+        return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
+    }
+
+    else {
+        return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
+    }
+}
+
+
 //#endregion
 
 //#region Operation events
