a8bc3b8c832b91a814da6b65025a73f1a8946acb
ALU52 <69333284+ALU52@users.noreply.github.com>
2020-10-19 08:49:44 -0500

    Update webapp.js

--- docs/webapp.js
+++ docs/webapp.js
@@ -1,25 +1,6 @@
 'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const e = React.createElement;
 
-class topNav extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { selected: false };
-    }
-
-    render() {
-        if (this.state.selected) {
-            return 'You selected this.';
-        }
-
-        return e(
-            'button',
-            { onClick: () => this.setState({ selected: true }) },
-            'Like'
-        );
-    }
-}
-
-const domContainer = document.querySelector('#appDisplay');
-ReactDOM.render(e(topNav), domContainer);
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById("appDisplay"));
